e5390433d185b0a97b8ac775ecc4b44d8aa1f62b
Mathias Rieder <Mathias.RIEDER@bachmann.info>
2023-11-28 18:04:29 +0100

    added tests and improved model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+import sys
+
 class Field:
     # Field class represents a single field on the map
     def __init__(self, x, y, elevation):
@@ -19,43 +21,99 @@ class Map:
     def __init__(self):
         # Initialize an empty list of fields
         self.fields = {}
+        self.height = 0
+        self.width = 0
+        self.start = None
+        self.end = None
 
     def set_start(self, x, y):
         # Set the start field at given x, y coordinates
         # dont forget to make sure that this field was added to the map prior
         # to calling this method (otherwise you will get a KeyError)
+        if self.start:
+            raise Exception("Multiple starting points detected!")
         self.start = self.get_field(x, y)
 
     def set_end(self, x, y):
         # Set the end field at given x, y coordinates
         # dont forget to make sure that this field was added to the map prior
         # to calling this method (otherwise you will get a KeyError)
+        if self.end:
+            raise Exception("Multiple ending points detected!")
         self.end = self.get_field(x, y)
 
     def add_field(self, x, y, elevation):
         # Add a new field to the map with given x, y coordinates and elevation
-        self.fields[(x,y)] = Field(x, y, elevation)
+        f = Field(x, y, elevation)
+        self.fields[(x,y)] = f
+        self.height = max(self.height, y+1)
+        self.width = max(self.width, x+1)
+        return f
 
     def get_field(self, x, y):
         # Get the field at given x, y coordinates
-        return self.fields[(x,y)]
+        if (x,y) in self.fields:
+            return self.fields[(x,y)]
+        else:
+            return Field(x, y, sys.maxsize)
 
     def get_neighbours(self, field):
         # Get a list of available neighbouring fields (N, S, W, E) of the given field
         # if they are not on the map, they are not available
-        pass
+        return (
+            self.get_field(field.x+0,field.y-1), #N
+            self.get_field(field.x+0,field.y+1), #S
+            self.get_field(field.x-1,field.y-0), #W
+            self.get_field(field.x+1,field.y-0)  #E
+        )
 
     @staticmethod
     def from_string(map_string):
+        map = Map()
         # Static method to create a Map object from a multiline string
-        pass
+        for y, line in enumerate(map_string.splitlines()):
+            for x, char in enumerate(line):
+                match  char:
+                    case 'S':
+                        map.add_field(x, y, 0)
+                        map.set_start(x, y)
+                    case 'E':
+                        map.add_field(x, y, 25)
+                        map.set_end(x, y)
+                    case _:
+                        if (char.isalpha() and char.islower()):
+                            map.add_field(x, y, ord(char)-ord('a'))
+                        else:
+                            raise Exception(f"Invalid cell detected at {(x, y)}!")
+
+        return map
 
 class Walker:
     # Walker class represents a walker with a position on the map
     def __init__(self, position):
         # Initialize the walker's position
-        self.position = position
-
+        self.path = Path()
+        self.path.add_step(position)
+        
+    def walk(self, field):
+        # Move the walker to the given field
+        self.path.add_step(field)
+        
+    def step_back(self):
+        # Move the walker back to the previous field
+        self.path.remove_last_step()
+        
+    def get_position(self):
+        # Return the walker's current position
+        return self.path.get_last_step()
+    
+    def has_walked(self, field):
+        # Return True if the walker has walked on the given field, False otherwise
+        return self.path.field_visited(field)
+    
+    def can_walk(self, field):
+        # Return True if the walker can walk to the given field, False otherwise
+        return field.elevation - self.get_position().elevation <= 1
 
 class Path:
     # Path class represents a sequence of fields forming a path on the map
@@ -71,7 +129,26 @@ class Path:
         # Remove the last step from the path
         if self.fields:
             self.fields.pop()
+            
+    def get_length(self):
+        # returns the number of steps in this path
+        return len(self.fields)
+    
+    def field_visited(self, field):
+        return field in self.fields
+    
+    def get_last_step(self):
+        return self.fields[-1]
 
+    def copy(self):
+        # returns a copy of this path
+        p = Path()
+        p.fields = self.fields.copy()
+        return p
+    
+    def __repr__(self):
+        # Return a string representation of the path
+        return f'Path({self.fields})'
 
 multilinestring = """\
 abc
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
 
 import pytest
-from src.model import Map, Field
+import sys
+from src.model import Map, Field, Path 
 
 def test_add_a_field_to_a_map():
     # GIVEN an empty map
@@ -47,3 +48,231 @@ b"""
     assert world.get_field(0, 0) == Field(0, 0, 0)
     # AND the map should contain a field with elevation 1 (=b) at (x=0, y=1)
     assert world.get_field(0, 1) == Field(0, 1, 1)
+
+def test_create_a_minimal_complex_twoline_map_from_string():
+    # GIVEN a multiline string
+    map_string = """\
+abbc
+bbaa
+caac"""
+    # WHEN creating a map from the string
+    world = Map.from_string(map_string)
+    # THEN the map should contain all fields with with the given elevation
+    assert world.get_field(0, 0) == Field(x=0, y=0, elevation=0)
+    assert world.get_field(0, 1) == Field(x=0, y=1, elevation=1)
+    assert world.get_field(0, 2) == Field(x=0, y=2, elevation=2)
+    assert world.get_field(1, 0) == Field(x=1, y=0, elevation=1)
+    assert world.get_field(1, 1) == Field(x=1, y=1, elevation=1)
+    assert world.get_field(1, 2) == Field(x=1, y=2, elevation=0)
+    assert world.get_field(2, 0) == Field(x=2, y=0, elevation=1)
+    assert world.get_field(2, 1) == Field(x=2, y=1, elevation=0)
+    assert world.get_field(2, 2) == Field(x=2, y=2, elevation=0)
+    assert world.get_field(3, 0) == Field(x=3, y=0, elevation=2)
+    assert world.get_field(3, 1) == Field(x=3, y=1, elevation=0)
+    assert world.get_field(3, 2) == Field(x=3, y=2, elevation=2)
+
+
+def test_create_a_map_with_start_and_end():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+    # WHEN creating a map from the string
+    world = Map.from_string(map_string)
+    # THEN the map should have the right starting and ending point, with elevation 0 at S and 25 at E
+    assert world.start  == Field(x=0, y=0, elevation=0)
+    assert world.end    == Field(x=5, y=2, elevation=25)
+
+def test_create_a_map_with_multiple_starts_and_one_end():
+    # GIVEN a multiline string with multiple starts and one end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+Scctuvwj
+abdefghi"""
+    # WHEN creating a map with multiple starting points
+        # THEN the map should throw an error that indicates too many starting points
+    with pytest.raises(Exception) as excinfo:
+        world = Map.from_string(map_string)
+    assert str(excinfo.value) == "Multiple starting points detected!"
+
+
+def test_create_a_map_with_one_start_and_multiple_ends():
+    # GIVEN a multiline string with multiple starts and one end
+    map_string = """\
+Sabqponm
+abcryxEl
+accszExk
+acctuvwj
+abdefghi"""
+    # WHEN creating a map with multiple end points
+    # THEN the map should throw an error that indicates too many end points
+
+    with pytest.raises(Exception) as excinfo:
+        world = Map.from_string(map_string)
+    assert str(excinfo.value) == "Multiple ending points detected!"
+
+def test_create_a_map_with_invalid_cell():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+a.mmlxl"""
+    # WHEN creating a map from the string
+    with pytest.raises(Exception) as excinfo:
+        world = Map.from_string(map_string)
+    assert str(excinfo.value) == "Invalid cell detected at (1, 0)!"
+
+def test_get_neightbors_in_middle():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+    world = Map.from_string(map_string)
+    # WHEN trying to get the neighboring fields
+    neighbor = world.get_neighbours(Field(x=2, y=2, elevation=2))
+    # THEN the map should return the right neighbors (N, S, W, E)
+    assert neighbor == (
+        Field(x=2, y=1, elevation=2), #north
+        Field(x=2, y=3, elevation=2), #south
+        Field(x=1, y=2, elevation=2), #west
+        Field(x=3, y=2, elevation=18) #east
+    )
+
+def test_get_neightbors_at_edge_x0_y0():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+    world = Map.from_string(map_string)
+    # WHEN trying to get the neighboring fields
+    neighbor = world.get_neighbours(Field(x=0, y=0, elevation=0))
+    # THEN the map should return the right neighbors (N, S, W, E)
+    assert neighbor == (
+        Field(x=0, y=-1, elevation=sys.maxsize), #north
+        Field(x=0, y=1, elevation=0), #south
+        Field(x=-1, y=0, elevation=sys.maxsize), #west
+        Field(x=1, y=0, elevation=0), #east
+        )
+
+def test_get_neightbors_at_edge_x_max_y_max():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+    world = Map.from_string(map_string)
+    # WHEN trying to get the neighboring fields
+    neighbor = world.get_neighbours(Field(x=7, y=4, elevation=ord('i')-ord('a')))
+    # THEN the map should return the right neighbors (N, S, W, E)
+    print(f"neighbor at edge max {neighbor}")
+    assert neighbor == (
+        Field(x=7, y=3, elevation=ord('j')-ord('a')), #north
+        Field(x=7, y=5, elevation=sys.maxsize), #south
+        Field(x=6, y=4, elevation=ord('h')-ord('a')), #west
+        Field(x=8, y=4, elevation=sys.maxsize), #east
+    )
+
+def test_get_neightbors_field_out_of_range_x():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+    world = Map.from_string(map_string)
+    # WHEN trying to get the neighboring fields of outside the map, x overrun
+    neighbor = world.get_neighbours(Field(x=8, y=0, elevation=0))
+    # THEN the map should return the right neighbors (N, S, W, E), which is None
+    assert neighbor == (
+        Field(x=8, y=-1, elevation=sys.maxsize), #north
+        Field(x=8, y=1, elevation=sys.maxsize), #south
+        Field(x=7, y=0, elevation=ord('m')-ord('a')), #west
+        Field(x=9, y=0, elevation=sys.maxsize), #east
+        )
+    
+def test_get_neightbors_field_out_of_range_y():
+    # GIVEN a multiline string with start and end
+    map_string = """\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+    world = Map.from_string(map_string)
+    # WHEN trying to get the neighboring fields of outside the map, y overrun
+    neighbor = world.get_neighbours(Field(x=0, y=5, elevation=0))
+    # THEN the map should return the right neighbors (N, S, W, E), which is None
+    assert neighbor == (
+        Field(x=0, y=4, elevation=0), #north
+        Field(x=0, y=6, elevation=sys.maxsize), #south
+        Field(x=-1, y=5, elevation=sys.maxsize), #west
+        Field(x=1, y=5, elevation=sys.maxsize), #east
+        )
+
+def test_an_empty_map():
+    # GIVEN an empty map
+    map_string = """\
+"""
+    # WHEN creating an empty map
+    world = Map.from_string(map_string)
+    # THEN check the width and the height of the map
+    assert world.height == 0
+    assert world.width == 0
+
+def test_add_2_steps():
+    # GIVEN a Path with 2 steps
+    p = Path()
+    #WHEN I add two steps
+    p.add_step(Field(0, 0, 77))
+    p.add_step(Field(0, 1, 77))
+    #THEN I expect a length of two and the coords (0,0), (0,1)
+    assert p.fields[0] == (Field(0, 0, 77))
+    assert p.fields[1] == (Field(0, 1, 77)) 
+    assert p.get_length() == 2
+            
+
+def test_visited_fields_visited():
+    # GIVEN A Path with  steps
+    p = Path()
+    p.add_step(Field(0, 0, 77))
+    p.add_step(Field(1, 0, 77))
+    p.add_step(Field(1, 1, 77))
+    # WHEN i asked if there was a step on a visited field
+    already_visited = p.field_visited(Field(1, 0,77))
+    # THEN the test should be true
+    assert already_visited == True
+
+def test_visited_fields_unvisited():
+    # GIVEN A Path with some steps
+    p = Path()
+    p.add_step(Field(0, 0, 77))
+    p.add_step(Field(1, 0, 77))
+    p.add_step(Field(1, 1, 77))
+    # WHEN i asked if i steped on a field already
+    already_visited = p.field_visited(Field(0, 3, 77))
+    # THEN the test should be false, because there was no step on a visited field
+    assert already_visited == False
+    
+def test_remove_last_step():
+    # GIVEN an empty Path 
+    p = Path()
+    # WHEN adding three steps (length = 3)
+    p.add_step(Field(0, 0, 10))
+    p.add_step(Field(1, 0, 10))
+    p.add_step(Field(2, 0, 10))    
+    # THEN the last step should be deleted
+    expected_fields = [(Field(0, 0, 10)), (Field(1, 0, 10))]
+    p.remove_last_step()
+    assert p.fields == expected_fields
